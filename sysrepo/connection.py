sysrepo/sysrepo-python

commit 2b1045d88285a3f1d85bb684b724e3a37c2f1abc

connection: ensure sr_disconnect is called
If a client program does not call disconnect() it leaves resources open
in sysrepo SHM. Use ffi.gc to register a destructor for the connection
pointer.

Since sr_connect can be a long operation (all YANG models must be
parsed), if a signal occurs at that time, ffi.gc will not be called and
no destructor will be registered. Block signals before calling
sr_connect() and unblock them just after registering sr_disconnect.

Signed-off-by: Robin Jarry <robin.jarry@6wind.com>

sysrepo/connection.py
@@ -2,6 +2,7 @@
 # SPDX-License-Identifier: BSD-3-Clause
 
 import logging
+import signal
 from typing import Optional, Sequence
 
 import libyang
@@ -60,8 +61,12 @@ class SysrepoConnection:
         if err_on_sched_fail:
             flags |= lib.SR_CONN_ERR_ON_SCHED_FAIL
         conn_p = ffi.new("sr_conn_ctx_t **")
-        check_call(lib.sr_connect, flags, conn_p)
-        self.cdata = conn_p[0]
+        sigmask = signal.pthread_sigmask(signal.SIG_BLOCK, range(1, signal.NSIG))
+        try:
+            check_call(lib.sr_connect, flags, conn_p)
+            self.cdata = ffi.gc(conn_p[0], lib.sr_disconnect)
+        finally:
+            signal.pthread_sigmask(signal.SIG_SETMASK, sigmask)
 
     def __enter__(self):
         return self
@@ -80,9 +85,9 @@ class SysrepoConnection:
         Connection and all its associated sessions and subscriptions can no longer be
         used even on error.
         """
-        try:
-            check_call(lib.sr_disconnect, self.cdata)
-        finally:
+        if self.cdata is not None:
+            if hasattr(ffi, "release"):
+                ffi.release(self.cdata)  # causes sr_disconnect to be called
             self.cdata = None
 
     def start_session(self, datastore: str = "running") -> SysrepoSession:

commit 29db1a2ce7bf2ed5e31fd7ed846ea04f07a5787f

connection: use valid_signals on python 3.8+
This avoids the following warning on python 3.8 on exotic platforms
(seen on Octeon TX):

  RuntimeWarning: invalid signal number 32, please use valid_signals()

Fixes: 2b1045d88285 ("connection: ensure sr_disconnect is called")
Signed-off-by: Robin Jarry <robin.jarry@6wind.com>

sysrepo/connection.py
@@ -61,7 +61,9 @@ class SysrepoConnection:
         if err_on_sched_fail:
             flags |= lib.SR_CONN_ERR_ON_SCHED_FAIL
         conn_p = ffi.new("sr_conn_ctx_t **")
-        sigmask = signal.pthread_sigmask(signal.SIG_BLOCK, range(1, signal.NSIG))
+        # valid_signals() is only available since python 3.8
+        valid_signals = getattr(signal, "valid_signals", lambda: range(1, signal.NSIG))
+        sigmask = signal.pthread_sigmask(signal.SIG_BLOCK, valid_signals())
         try:
             check_call(lib.sr_connect, flags, conn_p)
             self.cdata = ffi.gc(conn_p[0], lib.sr_disconnect)