sysrepo/sysrepo-python

commit 3073a2cfdb8662e65645a63ce628ad3ad4706b4c

subscription: fix action support for RPC callbacks
Actions are RPCs tied to a data node. See RFC 7950 for more details.

The RPC callback arguments have been changed to include the actual
operation path. For RPCs, it is the Xpath used when subscribing,
however, for actions, it is the full data path (see the docstring of
RpcCallbackType for more details).

To avoid tedious implementation of RPC/action callbacks, the output data
tree is already initialized by libsysrepo.so and only the actual output
leafs and containers need to be created. For that reason, callbacks
should now only return a dict with the output nodes (see the docstring
of RpcCallbackType, examples and tests).

In order to make this work, libyang>=1.5.0 is required.

Link: https://tools.ietf.org/html/rfc7950#section-7.15
Signed-off-by: Robin Jarry <robin.jarry@6wind.com>

examples/application_asyncio.py
@@ -113,10 +113,11 @@ async def oper_data_cb(xpath, private_data):
 
 
 # ------------------------------------------------------------------------------
-async def poweroff(rpc_input, event, private_data):
+async def poweroff(xpath, input_params, event, private_data):
     print()
     print("========================")
-    print("RPC call: %s" % rpc_input)
+    print("RPC call: %s" % xpath)
+    print("params: %s" % input_params)
     out = {"poweroff": {"message": "bye bye"}}
     print("returning %s" % out)
     print("---------------")

examples/application.py
@@ -11,6 +11,7 @@ import argparse
 import logging
 import signal
 import sys
+import time
 
 import sysrepo
 
@@ -46,6 +47,12 @@ def main():
                 )
                 logging.info("subscribing to rpc calls: /sysrepo-example:poweroff")
                 sess.subscribe_rpc_call("/sysrepo-example:poweroff", poweroff)
+                logging.info(
+                    "subscribing to action calls: /sysrepo-example:conf/security/alarm/trigger"
+                )
+                sess.subscribe_rpc_call(
+                    "/sysrepo-example:conf/security/alarm/trigger", trigger_alarm
+                )
                 signal.sigwait({signal.SIGINT, signal.SIGTERM})
         return 0
     except sysrepo.SysrepoError as e:
@@ -97,14 +104,29 @@ def oper_data_cb(xpath, private_data):
 
 
 # ------------------------------------------------------------------------------
-def poweroff(rpc_input, event, private_data):
+def poweroff(xpath, input_params, event, private_data):
     print()
     print("========================")
-    print("RPC call: %s" % rpc_input)
-    out = {"poweroff": {"message": "bye bye"}}
+    print("RPC call: %s" % xpath)
+    print("params: %s" % input_params)
+    out = {"message": "bye bye"}
     print("returning %s" % out)
     print("---------------")
     print()
+    return out
+
+
+def trigger_alarm(xpath, input_params, event, private_data):
+    print()
+    print("========================")
+    print("Action call: %s" % xpath)
+    print("params: %s" % input_params)
+    seconds = input_params["duration"]
+    out = {"message": "triggered for %s seconds" % seconds}
+    print("returning %s" % out)
+    print("---------------")
+    print()
+    time.sleep(seconds)
     return out
 
 

examples/sysrepo-example.yang
@@ -1,4 +1,5 @@
 module sysrepo-example {
+  yang-version 1.1;
   namespace n;
   prefix p;
 
@@ -33,6 +34,29 @@ module sysrepo-example {
 
   container conf {
     uses data;
+
+    container security {
+      list alarm {
+        key name;
+        leaf name {
+          type string;
+        }
+        action trigger {
+          input {
+            leaf duration {
+              type uint16;
+              units "seconds";
+              default 1;
+            }
+          }
+          output {
+            leaf message {
+              type string;
+            }
+          }
+        }
+      }
+    }
   }
 
   container state {

pylintrc
@@ -74,6 +74,7 @@ disable=
 	file-ignored,
 	fixme,
 	import-outside-toplevel,
+	line-too-long,
 	locally-disabled,
 	missing-docstring,
 	no-self-use,

setup.py
@@ -147,7 +147,7 @@ setuptools.setup(
         'cffi; platform_python_implementation != "PyPy"',
     ],
     install_requires=[
-        "libyang>=1.4.0",
+        "libyang>=1.5.0",
         'cffi; platform_python_implementation != "PyPy"',
     ],
     cffi_modules=["cffi/build.py:BUILDER"],

sysrepo/session.py
@@ -321,19 +321,45 @@ class SysrepoSession:
 
         self.subscriptions.append(sub)
 
-    RpcCallbackType = Callable[[Dict, str, Any], Optional[Dict]]
+    RpcCallbackType = Callable[[str, Dict, str, Any], Optional[Dict]]
     """
     Callback to be called when the RPC/action is invoked.
 
-    :arg rpc_input:
-        The RPC input arguments in a python dictionary. If there are any input
-        parameters, the first level of the dictionary will have only one key with the
-        name of the RPC::
+    :arg xpath:
+        The full data path to the invoked RPC/action. When it is an RPC, the form is
+        `/prefix:rpc-name`. When it is an action, it is the full data path with all
+        parent nodes: `/prefix:container/list[key="val"]/action-name`.
+    :arg input_params:
+        The input arguments in a python dictionary. The contents are limited to the
+        children of the "input" node. For example, with a YANG rpc defined like this::
 
-            {'rpc-name': {'param1': 42, 'param2': 'foobar'}}
+            rpc rpc-name {
+              input {
+                leaf param1 {
+                  type uint32;
+                }
+                leaf param2 {
+                  type string;
+                }
+              }
+              output {
+                leaf foo {
+                  type int8;
+                }
+                leaf bar {
+                  type string;
+                }
+              }
+            }
 
-        If there are no input parameters provided by the client, the dictionary will be
-        empty: ``{}``.
+        The input_params dict may look like this::
+
+            {'param1': 42, 'param2': 'foobar'}
+
+        If there are no input parameters provided by the client, the dict will be empty.
+
+        For actions, the xpath argument allows to determine the parent node of the
+        action input parameters.
     :arg event:
         In most cases, it is always 'rpc'. When multiple callbacks are registered for
         the same RPC and one of the callbacks failed. The remainder of the callbacks
@@ -342,11 +368,16 @@ class SysrepoSession:
         Private context opaque to sysrepo used when subscribing.
 
     The callback is expected to return a python dictionary containing the RPC output
-    data. The dictionary should be in the libyang "dict" format. It will be passed to
-    libyang.DRpc.merge_data_dict() to return output data to sysrepo. If the callback
-    returns None, nothing is returned to sysrepo. If the callback raises an exception,
-    the error message is forwarded to the client that called the RPC. If the exception
-    is a subclass of SysrepoError, no traceback is sent to the logging system.
+    data. The dictionary should be in the libyang "dict" format and must only contain
+    the actual output parameters without any parents. Using the previous example::
+
+        {'foo': 47, 'bar': 'baz'}
+
+    It will be passed to libyang.DRpc.merge_data_dict() to return output data to
+    sysrepo. If the callback returns None, nothing is returned to sysrepo. If the
+    callback raises an exception, the error message is forwarded to the client that
+    called the RPC. If the exception is a subclass of SysrepoError, no traceback is sent
+    to the logging system.
     """
 
     def subscribe_rpc_call(

sysrepo/subscription.py
@@ -10,7 +10,7 @@ from libyang.data import DNode
 
 from _sysrepo import ffi, lib
 from .errors import SysrepoError, SysrepoNotFoundError, check_call
-from .util import c2str, is_async_func, xpath_split
+from .util import c2str, is_async_func
 
 
 LOG = logging.getLogger(__name__)
@@ -410,9 +410,15 @@ def rpc_callback(session, xpath, input_node, event, req_id, output_node, priv):
 
         session = SysrepoSession(session, True)
         ly_ctx = session.get_ly_ctx()
-        xpath = c2str(xpath)
-        input_dict = DNode.new(ly_ctx, input_node).print_dict(
-            include_implicit_defaults=True
+        rpc_input = DNode.new(ly_ctx, input_node)
+        xpath = rpc_input.path()
+        # strip all parents, only preserve the input tree
+        input_dict = next(
+            iter(
+                rpc_input.print_dict(
+                    include_implicit_defaults=True, absolute=False
+                ).values()
+            )
         )
         subscription = ffi.from_handle(priv)
         callback = subscription.callback
@@ -424,7 +430,7 @@ def rpc_callback(session, xpath, input_node, event, req_id, output_node, priv):
 
             if task_id not in subscription.tasks:
                 task = subscription.loop.create_task(
-                    callback(input_dict, event_name, private_data)
+                    callback(xpath, input_dict, event_name, private_data)
                 )
                 task.add_done_callback(
                     functools.partial(subscription.task_done, task_id, event_name)
@@ -441,7 +447,7 @@ def rpc_callback(session, xpath, input_node, event, req_id, output_node, priv):
             output_dict = task.result()
 
         else:
-            output_dict = callback(input_dict, event_name, private_data)
+            output_dict = callback(xpath, input_dict, event_name, private_data)
 
         if event != lib.SR_EV_RPC:
             # May happen when there are multiple callback registered for the
@@ -452,15 +458,7 @@ def rpc_callback(session, xpath, input_node, event, req_id, output_node, priv):
 
         if isinstance(output_dict, dict):
             # update output_node with contents of output_dict
-            # strip first level of output_dict with the rpc name
-            prefix, name, _ = next(xpath_split(xpath))
-            name_prefix = "%s:%s" % (prefix, name)
-            if name in output_dict:
-                output_dict = output_dict[name]
-            elif name_prefix in output_dict:
-                output_dict = output_dict[name_prefix]
-            dnode = DNode.new(ly_ctx, output_node)
-            dnode.merge_data_dict(
+            DNode.new(ly_ctx, output_node).merge_data_dict(
                 output_dict, rpcreply=True, strict=subscription.strict, validate=False
             )
         elif output_dict is not None:

tests/test_subs_rpc.py
@@ -1,8 +1,11 @@
 # Copyright (c) 2020 6WIND S.A.
 # SPDX-License-Identifier: BSD-3-Clause
 
+import copy
 import os
 import unittest
+
+import libyang
 
 import sysrepo
 
@@ -31,39 +34,103 @@ class RpcSubscriptionTest(unittest.TestCase):
     def test_rpc_sub(self):
         priv = object()
         calls = []
+        rpc_xpath = "/sysrepo-example:poweroff"
 
-        def rpc_cb(rpc_input, event, private_data):
-            calls.append((rpc_input, event, private_data))
-            behaviour = rpc_input.get("poweroff", {}).get("behaviour")
+        def rpc_cb(xpath, input_params, event, private_data):
+            calls.append((xpath, input_params, event, private_data))
+            behaviour = input_params.get("behaviour")
             if behaviour == "failure":
                 raise sysrepo.SysrepoCallbackFailedError("poweroff failed")
             if behaviour == "bad-output":
-                return {"poweroff": {"unkown-leaf": False}}
-            return {"poweroff": {"message": "bye bye"}}
+                return {"unkown-leaf": False}
+            return {"message": "bye bye"}
 
         with self.conn.start_session() as sess:
-            sess.subscribe_rpc_call(
-                "/sysrepo-example:poweroff", rpc_cb, private_data=priv, strict=True
-            )
+            sess.subscribe_rpc_call(rpc_xpath, rpc_cb, private_data=priv, strict=True)
 
             with self.conn.start_session() as rpc_sess:
                 rpc = {"poweroff": {"behaviour": "failure"}}
                 with self.assertRaises(sysrepo.SysrepoCallbackFailedError):
                     rpc_sess.rpc_send(rpc, "sysrepo-example")
                 self.assertEqual(len(calls), 1)
-                self.assertEqual(calls[0], (rpc, "rpc", priv))
+                self.assertEqual(calls[0], (rpc_xpath, rpc["poweroff"], "rpc", priv))
                 del calls[:]
 
                 rpc = {"poweroff": {"behaviour": "bad-output"}}
                 with self.assertRaises(sysrepo.SysrepoCallbackFailedError):
                     rpc_sess.rpc_send(rpc, "sysrepo-example")
                 self.assertEqual(len(calls), 1)
-                self.assertEqual(calls[0], (rpc, "rpc", priv))
+                self.assertEqual(calls[0], (rpc_xpath, rpc["poweroff"], "rpc", priv))
                 del calls[:]
 
                 rpc = {"poweroff": {"behaviour": "success"}}
                 output = rpc_sess.rpc_send(rpc, "sysrepo-example")
                 self.assertEqual(output, {"poweroff": {"message": "bye bye"}})
                 self.assertEqual(len(calls), 1)
-                self.assertEqual(calls[0], (rpc, "rpc", priv))
+                self.assertEqual(calls[0], (rpc_xpath, rpc["poweroff"], "rpc", priv))
+                del calls[:]
+
+    def test_action_sub(self):
+        priv = object()
+        calls = []
+        subs_xpath = "/sysrepo-example:conf/security/alarm/trigger"
+
+        def action_cb(xpath, input_params, event, private_data):
+            calls.append((xpath, input_params, event, private_data))
+            duration = input_params["duration"]
+            return {"message": "triggered for %r seconds" % (duration,)}
+
+        def module_change_cb(event, req_id, config, changes, private_data):
+            # unused
+            pass
+
+        with self.conn.start_session() as sess:
+            sess.subscribe_rpc_call(
+                subs_xpath, action_cb, private_data=priv, strict=True
+            )
+            sess.replace_config(
+                {
+                    "conf": {
+                        "security": {
+                            "alarm": [
+                                {"name": "lab1"},
+                                {"name": "lab2"},
+                                {"name": "office1"},
+                            ],
+                        },
+                    },
+                },
+                "sysrepo-example",
+            )
+            # Subscribe to data changes so that config is present in operational
+            # datastore. This is required for action input validation.
+            sess.subscribe_module_change("sysrepo-example", None, module_change_cb)
+
+            with self.conn.start_session() as rpc_sess:
+                action = {
+                    "conf": {
+                        "security": {
+                            "alarm": [{"name": "lab2", "trigger": {"duration": 30}}],
+                        },
+                    },
+                }
+                output = rpc_sess.rpc_send(action, "sysrepo-example")
+                call_xpath = "/sysrepo-example:conf/security/alarm[name='lab2']/trigger"
+                expected_output = copy.deepcopy(action)
+                libyang.xpath_set(
+                    expected_output, call_xpath, {"message": "triggered for 30 seconds"}
+                )
+                self.assertEqual(output, expected_output)
+                self.assertEqual(len(calls), 1)
+                self.assertEqual(calls[0], (call_xpath, {"duration": 30}, "rpc", priv))
+                del calls[:]
+
+                # no value for duration, check default value is set
+                libyang.xpath_set(action, call_xpath, {})
+                output = rpc_sess.rpc_send(action, "sysrepo-example")
+                libyang.xpath_set(
+                    expected_output, call_xpath, {"message": "triggered for 1 seconds"}
+                )
+                self.assertEqual(len(calls), 1)
+                self.assertEqual(calls[0], (call_xpath, {"duration": 1}, "rpc", priv))
                 del calls[:]

commit 70da251b43eb44344a1f047bf2b0219973702f07

examples: fix asyncio rpc example
The rpc name should not be returned anymore.

Fixes: #14
Fixes: 3073a2cfdb86 ("subscription: fix action support for RPC callbacks")
Signed-off-by: Robin Jarry <robin@jarry.cc>

examples/application_asyncio.py
@@ -128,7 +128,7 @@ async def poweroff(xpath, input_params, event, private_data):
     print("========================")
     print("RPC call: %s" % xpath)
     print("params: %s" % input_params)
-    out = {"poweroff": {"message": "bye bye"}}
+    out = {"message": "bye bye"}
     print("returning %s" % out)
     print("---------------")
     print()