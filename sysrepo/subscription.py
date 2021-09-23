sysrepo/sysrepo-python

commit 7b9426d75328ccab8fdc7a39dd924ea6cfa6b950

Add support for notifications
* Add method to register a notification callback

* Add method to send a notification

* Related to:
  - sysrepo/sysrepo-python#10
  - sysrepo/sysrepo-python#11

* Special recognition to @ishidawataru for providing the seed
  code for this new feature

sysrepo/subscription.py
@@ -160,6 +160,14 @@ EVENT_NAMES = {
     lib.SR_EV_RPC: "rpc",
 }
 
+NOTIF_TYPES = {
+    lib.SR_EV_NOTIF_REALTIME: "realtime",
+    lib.SR_EV_NOTIF_REPLAY: "replay",
+    lib.SR_EV_NOTIF_REPLAY_COMPLETE: "replay_complete",
+    lib.SR_EV_NOTIF_STOP: "stop",
+    lib.SR_EV_NOTIF_SUSPENDED: "suspended",
+    lib.SR_EV_NOTIF_RESUMED: "resumed",
+}
 
 # ------------------------------------------------------------------------------
 @ffi.def_extern(name="srpy_module_change_cb")
@@ -487,3 +495,60 @@ def rpc_callback(session, xpath, input_node, event, req_id, output_node, priv):
         if isinstance(session, SysrepoSession) and isinstance(xpath, str):
             session.set_error(xpath, str(e))
         return lib.SR_ERR_CALLBACK_FAILED
+
+
+# ------------------------------------------------------------------------------
+@ffi.def_extern(name="srpy_event_notif_tree_cb")
+def event_notif_tree_callback(session, notif_type, notif, timestamp, priv):
+    """
+    Callback to be called when a notification is received.
+
+    :arg "sr_session_ctx_t *" session:
+        Implicit session (do not stop).
+    :arg "sr_ev_notif_type_t" notif_type:
+        Type of the notification event that has occurred.
+    :arg "const struct lyd_node *" notif:
+        Data tree of input parameters.
+    :arg "uint32_t" timestamp:
+        Timestamp of the notification.
+    :arg "void *" priv:
+        Private context opaque to sysrepo. Contains a CFFI handle to the Subscription
+        python object.
+
+    :returns:
+        User error code (sr_error_t).
+    :raises:
+        IMPORTANT: This function *CANNOT* raise any exception. The C callstack does not
+        handle that well and when it happens the outcome is undetermined. Make sure to
+        catch all errors and log them so they are not lost.
+    """
+    try:
+        # convert C arguments to python objects.
+        from .session import SysrepoSession  # circular import
+
+        session = SysrepoSession(session, True)
+        subscription = ffi.from_handle(priv)
+        callback = subscription.callback
+        private_data = subscription.private_data
+        notif_type = NOTIF_TYPES[notif_type]
+
+        ly_ctx = session.get_ly_ctx()
+        notif_dnode = DNode.new(ly_ctx, notif)
+        xpath = notif_dnode.path()
+        notif_dict = next(
+            iter(
+                notif_dnode.print_dict(
+                    include_implicit_defaults=subscription.include_implicit_defaults,
+                    absolute=False,
+                ).values()
+            )
+        )
+        callback(xpath, notif_type, notif_dict, timestamp, private_data)
+
+    except BaseException as e:
+        # ATTENTION: catch all exceptions!
+        # including KeyboardInterrupt, CancelledError, etc.
+        # We are in a C callback, we cannot let any error pass
+        LOG.exception("%r callback failed", locals().get("callback", priv))
+        if isinstance(session, SysrepoSession) and isinstance(xpath, str):
+            session.set_error(xpath, str(e))

cffi/cdefs.h
@@ -244,3 +244,23 @@ int sr_oper_get_items_subscribe(
 	sr_session_ctx_t *, const char *module, const char *xpath,
 	int (*)(sr_session_ctx_t *, const char *, const char *, const char *, uint32_t, struct lyd_node **, void *),
 	void *priv, sr_subscr_options_t, sr_subscription_ctx_t **);
+
+typedef enum sr_ev_notif_type_e {
+	SR_EV_NOTIF_REALTIME,
+	SR_EV_NOTIF_REPLAY,
+	SR_EV_NOTIF_REPLAY_COMPLETE,
+	SR_EV_NOTIF_STOP,
+	SR_EV_NOTIF_SUSPENDED,
+	SR_EV_NOTIF_RESUMED,
+	...
+} sr_ev_notif_type_t;
+
+extern "Python" void srpy_event_notif_tree_cb(
+	sr_session_ctx_t *, const sr_ev_notif_type_t notif_type, const struct lyd_node *notif,
+	time_t timestamp, void *priv);
+
+int sr_event_notif_subscribe_tree(sr_session_ctx_t *, const char *module_name, const char *xpath, time_t start_time, time_t stop_time,
+	void (*)(sr_session_ctx_t *, const sr_ev_notif_type_t, const struct lyd_node*, time_t, void*),
+	void *priv, sr_subscr_options_t, sr_subscription_ctx_t **);
+
+int sr_event_notif_send_tree(sr_session_ctx_t *, struct lyd_node *notif);

examples/sysrepo-example.yang
@@ -74,6 +74,11 @@ module sysrepo-example {
         }
       }
     }
+    notification state-changed {
+      leaf message {
+        type string;
+      }
+    }
   }
 
   rpc poweroff {
@@ -90,6 +95,15 @@ module sysrepo-example {
       leaf message {
         type string;
       }
+    }
+  }
+
+  notification alarm-triggered {
+    leaf description {
+      type string;
+    }
+    leaf severity {
+      type uint32;
     }
   }
 }

setup.py
@@ -154,7 +154,7 @@ setuptools.setup(
         'cffi; platform_python_implementation != "PyPy"',
     ],
     install_requires=[
-        "libyang>=1.5,<2",
+        "libyang>=1.7.0,<2",
         'cffi; platform_python_implementation != "PyPy"',
     ],
     cffi_modules=["cffi/build.py:BUILDER"],

sysrepo/session.py
@@ -450,6 +450,107 @@ class SysrepoSession:
 
         self.subscriptions.append(sub)
 
+    NotificationCallbackType = Callable[[str, str, Dict, int, Any], None]
+    """
+    Callback to be called when a notification is received.
+
+    :arg xpath:
+        The full xpath to the received notification.
+    :arg notification_type:
+        Type of the notification event. Can be one of: "realtime", "replay",
+        "replay_complete", "stop", "suspended", "resumed".
+    :arg notification:
+        The notification as a python dictionary. For example, with a YANG notification
+        defined like this::
+
+            notification some-notification {
+              leaf param1 {
+                type uint32;
+              }
+              leaf param2 {
+                type string;
+              }
+            }
+
+        The notification dict may look like this::
+
+            {'param1': 42, 'param2': 'foobar'}
+
+    :arg timestamp:
+        Timestamp of the notification as an unsigned 32-bits integer.
+    :arg private_data:
+        Private context opaque to sysrepo used when subscribing.
+    """
+
+    def subscribe_notification(
+        self,
+        module: str,
+        xpath: str,
+        callback: NotificationCallbackType,
+        *,
+        start_time: int = 0,
+        stop_time: int = 0,
+        no_thread: bool = False,
+        asyncio_register: bool = False,
+        private_data: Any = None
+    ) -> None:
+        """
+        Subscribe for the delivery of a notification.
+
+        :arg module:
+            Name of the module whose notifications to subscribe to.
+        :arg xpath:
+            XPath identifying the notification.
+        :arg callback:
+            Callback to be called when the notification is received.
+        :arg start_time:
+            Optional start time of the subscription. Used for replaying stored
+            notifications.
+        :arg stop_time:
+            Optional stop time ending the notification subscription.
+        :arg no_thread:
+            There will be no thread created for handling this subscription meaning no
+            event will be processed! Default to True if asyncio_register is True.
+        :arg asyncio_register:
+            Add the created subscription event pipe into asyncio event loop monitored
+            read file descriptors. Implies no_thread=True.
+        :arg private_data:
+            Private context passed to the callback function, opaque to sysrepo.
+        """
+
+        if self.is_implicit:
+            raise SysrepoUnsupportedError("cannot subscribe with implicit sessions")
+        _check_subscription_callback(callback, self.NotificationCallbackType)
+
+        sub = Subscription(
+            callback,
+            private_data,
+            asyncio_register=asyncio_register,
+        )
+
+        sub_p = ffi.new("sr_subscription_ctx_t **")
+
+        if asyncio_register:
+            no_thread = True  # we manage our own event loop
+
+        flags = _subscribe_flags(no_thread=no_thread)
+
+        check_call(
+            lib.sr_event_notif_subscribe_tree,
+            self.cdata,
+            str2c(module),
+            str2c(xpath),
+            start_time,
+            stop_time,
+            lib.srpy_event_notif_tree_cb,
+            sub.handle,
+            flags,
+            sub_p,
+        )
+        sub.init(sub_p[0])
+
+        self.subscriptions.append(sub)
+
     # end: subscription
 
     # begin: changes
@@ -999,6 +1100,54 @@ class SysrepoSession:
             return next(iter(out_dict.values()))
         finally:
             out_dnode.free()
+
+    def notification_send_ly(self, notification: libyang.DNode) -> None:
+        """
+        Send a notification
+
+        :arg notification:
+            The notification tree. It is *NOT* spent and must be freed by the
+            caller.
+
+        :raises SysrepoError:
+            If the notification callback failed.
+        """
+        if not isinstance(notification, libyang.DNode):
+            raise TypeError("notification must be a libyang.DNode")
+        # libyang and sysrepo bindings are different, casting is required
+        in_dnode = ffi.cast("struct lyd_node *", notification.cdata)
+        check_call(lib.sr_event_notif_send_tree, self.cdata, in_dnode)
+
+    def notification_send(
+        self, xpath: str, notification: Dict, strict: bool = False
+    ) -> None:
+        """
+        Same as notification_send_ly() but takes a python dictionary as a notification.
+
+        :arg xpath:
+            The xpath corresponding to the notification
+        :arg notification:
+            The notification to send represented as a dictionary
+        :arg strict:
+            If True, reject notification if it contains elements without any schema
+            definition.
+
+        :raises SysrepoError:
+            If the notification callback failed.
+        """
+
+        ctx = self.get_ly_ctx()
+        full_notification = {}
+        libyang.xpath_set(full_notification, xpath, notification)
+        module_name, _, _ = next(libyang.xpath_split(xpath))
+        module = ctx.get_module(module_name)
+        dnode = module.parse_data_dict(
+            full_notification, notification=True, strict=strict, validate=False
+        )
+        try:
+            self.notification_send_ly(dnode)
+        finally:
+            dnode.free()
 
 
 # -------------------------------------------------------------------------------------

tests/test_subs_notification.py
@@ -1,0 +1,70 @@
+# Copyright (c) 2020 6WIND S.A.
+# SPDX-License-Identifier: BSD-3-Clause
+
+import logging
+import os
+import threading
+import time
+import typing
+import unittest
+
+import sysrepo
+
+
+YANG_FILE = os.path.join(
+    os.path.dirname(os.path.dirname(__file__)), "examples/sysrepo-example.yang"
+)
+sysrepo.configure_logging(stderr_level=logging.ERROR)
+
+
+class NotificationSubscriptionTest(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        with sysrepo.SysrepoConnection() as conn:
+            conn.install_module(YANG_FILE, enabled_features=["turbo"])
+        cls.conn = sysrepo.SysrepoConnection(err_on_sched_fail=True)
+
+    @classmethod
+    def tearDownClass(cls):
+        cls.conn.remove_module("sysrepo-example")
+        cls.conn.disconnect()
+        # reconnect to make sure module is removed
+        with sysrepo.SysrepoConnection(err_on_sched_fail=True):
+            pass
+
+    def _test_notification_sub(self, notif_xpath: str, notif_dict: typing.Dict):
+        priv = object()
+        callback_called = threading.Event()
+
+        def notif_cb(xpath, notification_type, notification, timestamp, private_data):
+            self.assertEqual(xpath, notif_xpath)
+            self.assertEqual(notification_type, "realtime")
+            self.assertEqual(notification, notif_dict)
+            self.assertIsInstance(timestamp, int)
+            self.assertAlmostEqual(timestamp, int(time.time()), delta=5)
+            self.assertEqual(private_data, priv)
+
+            callback_called.set()
+
+        with self.conn.start_session() as sess:
+            sess.subscribe_notification(
+                "sysrepo-example", notif_xpath, notif_cb, private_data=priv
+            )
+
+            sess.notification_send(notif_xpath, notif_dict)
+            self.assertTrue(
+                callback_called.wait(timeout=1),
+                "Timed-out while waiting for the notification callback to be called",
+            )
+
+    def test_notification_top_level(self):
+        self._test_notification_sub(
+            notif_xpath="/sysrepo-example:alarm-triggered",
+            notif_dict={"description": "An error occurred", "severity": 3},
+        )
+
+    def test_notification_nested_in_data_node(self):
+        self._test_notification_sub(
+            notif_xpath="/sysrepo-example:state/state-changed",
+            notif_dict={"message": "Some state changed"},
+        )

commit 140c693466d8b7520e68f13303fd8d9ea03ddb32

subscription: fix async notifications callbacks
When the callback is a coroutine, it must be scheduled in the event loop
for execution. sysrepo does not care about the result of the callback.

Fixes: #18
Fixes: 7b9426d75328 ("Add support for notifications")
Signed-off-by: Robin Jarry <robin@jarry.cc>

sysrepo/subscription.py
@@ -543,7 +543,17 @@ def event_notif_tree_callback(session, notif_type, notif, timestamp, priv):
                 ).values()
             )
         )
-        callback(xpath, notif_type, notif_dict, timestamp, private_data)
+        if is_async_func(callback):
+            task = subscription.loop.create_task(
+                callback(xpath, notif_type, notif_dict, timestamp, private_data)
+            )
+            task.add_done_callback(
+                functools.partial(subscription.task_done, None, "notif")
+            )
+        else:
+            callback(xpath, notif_type, notif_dict, timestamp, private_data)
+
+        return lib.SR_ERR_OK
 
     except BaseException as e:
         # ATTENTION: catch all exceptions!
@@ -552,3 +562,4 @@ def event_notif_tree_callback(session, notif_type, notif, timestamp, priv):
         LOG.exception("%r callback failed", locals().get("callback", priv))
         if isinstance(session, SysrepoSession) and isinstance(xpath, str):
             session.set_error(xpath, str(e))
+        return lib.SR_ERR_CALLBACK_FAILED