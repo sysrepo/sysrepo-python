sysrepo/sysrepo-python

commit 382a07cc8e35b1dd939989205bd46b524822c23a

subscription: fix async notifications callbacks
When the callback is a coroutine, it must be scheduled in the event loop
for execution. sysrepo does not care about the result of the callback.

Fixes: #18
Fixes: 7b9426d75328 ("Add support for notifications")
Signed-off-by: Robin Jarry <robin@jarry.cc>

sysrepo/subscription.py
@@ -543,7 +543,17 @@ def event_notif_tree_callback(session, notif_type, notif, timestamp, priv):
                 ).values()
             )
         )
-        callback(xpath, notif_type, notif_dict, timestamp, private_data)
+        if is_async_func(callback):
+            task = subscription.loop.create_task(
+                callback(xpath, notif_type, notif_dict, timestamp, private_data)
+            )
+            task.add_done_callback(
+                functools.partial(subscription.task_done, None, "notif")
+            )
+        else:
+            callback(xpath, notif_type, notif_dict, timestamp, private_data)
+
+        return lib.SR_ERR_OK
 
     except BaseException as e:
         # ATTENTION: catch all exceptions!
@@ -552,3 +562,4 @@ def event_notif_tree_callback(session, notif_type, notif, timestamp, priv):
         LOG.exception("%r callback failed", locals().get("callback", priv))
         if isinstance(session, SysrepoSession) and isinstance(xpath, str):
             session.set_error(xpath, str(e))
+        return lib.SR_ERR_CALLBACK_FAILED

commit d1ad0b8d79bd4fee67b4cbf74fad81216b11d73a

subscription: remove notification callback return value
Fix the following CFFI error:

 From cffi callback <function event_notif_tree_callback>:
 Trying to convert the result back to C:
 TypeError: callback with the return type 'void' must return None

Do not return anything and avoid sending the error to the sysrepo
session.

Fixes: #18
Fixes: 382a07cc8e35 ("subscription: fix async notifications callbacks")
Signed-off-by: Robin Jarry <robin@jarry.cc>

sysrepo/subscription.py
@@ -516,7 +516,7 @@ def event_notif_tree_callback(session, notif_type, notif, timestamp, priv):
         python object.
 
     :returns:
-        User error code (sr_error_t).
+        None
     :raises:
         IMPORTANT: This function *CANNOT* raise any exception. The C callstack does not
         handle that well and when it happens the outcome is undetermined. Make sure to
@@ -553,13 +553,8 @@ def event_notif_tree_callback(session, notif_type, notif, timestamp, priv):
         else:
             callback(xpath, notif_type, notif_dict, timestamp, private_data)
 
-        return lib.SR_ERR_OK
-
-    except BaseException as e:
+    except BaseException:
         # ATTENTION: catch all exceptions!
         # including KeyboardInterrupt, CancelledError, etc.
         # We are in a C callback, we cannot let any error pass
         LOG.exception("%r callback failed", locals().get("callback", priv))
-        if isinstance(session, SysrepoSession) and isinstance(xpath, str):
-            session.set_error(xpath, str(e))
-        return lib.SR_ERR_CALLBACK_FAILED